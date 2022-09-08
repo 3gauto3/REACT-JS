18bacc724529e79de9bd0f828a12c498ce0d49c1
Bryan Alejandro Gauto Gauto <bryanalejandrogautogauto@gmail.com>
2022-09-07 21:30:28 -0300

    segundo commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <nav>
+            <h1>Aranduka</h1>
+      <ul>
+        <li>
+        <a>Inicio</a>
+        </li>
+         <li>
+          <a>Géneros</a>
+        </li>
+         <li>
+          <a>Iniciar sesión</a>
+        </li>
+         <li>
+         <a>Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default App;
